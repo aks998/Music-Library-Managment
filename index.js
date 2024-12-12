cdeffa3f26c952e382d79843e5aed3c5bc6369ee
Arun <awesomearun.998@gmail.com>
2024-12-12 21:02:06 +0530

    Basics

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const connection = require('./connection/sqlDBConnection')
+
+connection.connect((err) => {
+    if(err) {
+        console.error(`Unable to connect to the database ${err.message}`)
+    }
+    else console.log('Connected to database successfully')
+})
 
 app.get('/' , (req , res) => {
     res.send("Hello world")
