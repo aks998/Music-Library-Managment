71c64c3a00264ecd974e714e4b06ec6d152088e2
Arun <awesomearun.998@gmail.com>
2024-12-13 00:20:50 +0530

    Adding Databae Connection

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 8080
 const createTables = require('./db/tables')
 
-createTables()
+await createTables()
 
 app.get('/' , (req , res) => {
+    console.log("THE IP ADDRESS " , req.ip)
     res.send("Hello world")
 })
 
