463a1c432c4185740dd486167cf1e498384cbc40
Arun <awesomearun.998@gmail.com>
2024-12-14 00:12:00 +0530

    Login AddUser SignUP

--- routes/addUser.js
+++ routes/addUser.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const sql = require('mssql')
 const crypto = require('crypto')
 const connectToDatabase = require('../db/sqlDBConnection');
--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../db/sqlDBConnection');
 const asyncHandler = require('express-async-handler')
